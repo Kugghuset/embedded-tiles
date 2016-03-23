0bbf31d5717a789419e7dc117165a9f92b685939
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-03-23 20:52:38 +0100

    Fix casing on azure client id

--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ export default {
   port: process.env.PORT || 3000,
   ip: process.env.IP || 'localhost',
   azure_domain: process.env.AZURE_DOMAIN || '',
-  azure_client_id: process.env.azure_client_id || '',
+  azure_client_id: process.env.AZURE_CLIENT_ID || '',
   azure_client_username: process.env.AZURE_CLIENT_USERNAME || '',
   azure_client_password: process.env.AZURE_CLIENT_PASSWORD || '',
   app_secret: process.env.APP_SECRET || '',
